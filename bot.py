a5a296ff916cd29408eeecacc37cc92c4b033577
Gabriel Castro <buguimo321@outlook.com>
2024-03-04 22:11:33 -0300

    Bot Update Colors

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import pygame.freetype
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
-
+import matplotlib.cm as cm
 
 pygame.init()
 
@@ -21,6 +21,10 @@ y, sr = librosa.load(music_file)
 D = np.abs(librosa.stft(y))
 S = librosa.amplitude_to_db(D, ref=np.max)
 
+S_color = cm.viridis((S - S.min()) / (S.max() - S.min()))
+
+pygame.mixer.music.load(music_file)
+pygame.mixer.music.play(-1)
 
 running = True
 clock = pygame.time.Clock()
@@ -30,23 +34,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
     screen.fill((0, 0, 0))
 
-
-    frame = pygame.surfarray.make_surface(S[:, :800])
-
-
+    frame = pygame.surfarray.make_surface(S_color[:, :800, :3] * 255)
     frame = pygame.transform.scale(frame, (width, height))
-
-
     screen.blit(frame, (0, 0))
-
-
     pygame.display.flip()
 
-
     clock.tick(30)
-
 
 pygame.quit()
