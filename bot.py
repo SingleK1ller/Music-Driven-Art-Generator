bdc574f6c548b7cee80d84276a0c116d8b26dffc
Gabriel Castro <buguimo321@outlook.com>
2024-03-09 03:18:46 -0300

    new framerate and animation

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@ import pygame.freetype
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
-import random
-
 import matplotlib.cm as cm
 
 pygame.init()
@@ -14,10 +12,10 @@ width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Music-Driven Art Visualizer')
 music_file = 'Tax Evasion.mp3'
+
 y, sr = librosa.load(music_file)
 D = np.abs(librosa.stft(y))
 S = librosa.amplitude_to_db(D, ref=np.max)
-y_harmonic, y_percussive = librosa.effects.hpss(y)
 
 S_color = cm.viridis((S - S.min()) / (S.max() - S.min()))
 
@@ -32,7 +30,7 @@ beat_times = librosa.frames_to_time(beat_frames, sr=sr)
 running = True
 clock = pygame.time.Clock()
 
-waveform_change_y = 0
+waveform_change_y = 800
 waveform_change_x = 0
 
 cooldown = 50
@@ -48,18 +46,17 @@ while running:
     now = pygame.time.get_ticks()
 
     if now - last >= cooldown:
+
         last = now
-        #waveform_change_y = random.randint(10, 800)
-        #waveform_change_x = random.randint(10, 500)
+
         if waveform_change_x > 800:
             pass
         else:
-            waveform_change_y += 1
+            waveform_change_y -= 1
             waveform_change_x += 1
 
 
     screen.fill((0, 0, 0))
-
 
 
     frame = pygame.surfarray.make_surface(S_color[:waveform_change_x, :waveform_change_y, :3] * 255)
@@ -72,7 +69,7 @@ while running:
     pygame.display.flip()
 
 
-    clock.tick(30)
+    clock.tick(10)
 
 
 pygame.quit()
